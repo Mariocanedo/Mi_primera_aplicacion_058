d1958ebc6403677f7cecc2bd97d0455dfb3774d1
StudioOne <Mario_cane.do@hotmail.com>
2023-05-24 17:27:48 -0400

    Primer programa 058

--- app/src/main/java/com/example/mi_primera_aplicacion_058/MainActivity.java
+++ app/src/main/java/com/example/mi_primera_aplicacion_058/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mi_primera_aplicacion_058;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // buscar y enlazar la vista
+
+        TextView textView = findViewById(R.id.textView4);
+        textView.setText("Este textview esta seteado con otro valor");
+
     }
 }
